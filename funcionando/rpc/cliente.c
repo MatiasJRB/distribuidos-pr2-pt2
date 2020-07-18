1a460f6fe77c706bddebac4a5e22b72a322e3277
MatiasJRB <mjrcal95@hotmail.com>
2020-07-17 21:32:20 -0300

    cd sin funcionalidad -- ni actualizar donde estoy parado

--- funcionando/rpc/cliente.c
+++ funcionando/rpc/cliente.c
@@ -26,7 +26,7 @@ CLIENT *clnt;
 /*Declara funciones*/
 void separarArgumentos();
 void listarDirectorio();
-void cambiarDirectorio();
+void ejecutarCD();
 void editor();
 
 
@@ -37,6 +37,7 @@ typedef struct{
     uint size;
 }SubDirectorio;
 SubDirectorio sd_actual;
+SubDirectorio sd_padre;
 
 
 /*
@@ -47,7 +48,7 @@ typedef struct{
     SubDirectorio sub_directorios[2];
     int cont;
 }PWD;
-
+PWD ruta;
 
 /* A partir de un PWD obtengo en string la ruta completa */
 
@@ -92,7 +93,7 @@ int main(int argc, char *argv[]){
     // downloadFile("192.168.0.186", "Makefile", "puto");
     
     int seguir=1;
-    PWD ruta;
+ 
 
     SubDirectorio raiz;    	
     raiz.name = (char*) malloc(sizeof(char)*5); /* Reservo lugar para '/' y '\0' */
@@ -102,6 +103,7 @@ int main(int argc, char *argv[]){
     ruta.cont = 1;
     ruta.sub_directorios[0] = raiz;
     sd_actual = raiz;    
+    sd_padre = raiz;
     
     //path = getPath(&ruta);
 
@@ -119,7 +121,9 @@ int main(int argc, char *argv[]){
                 seguir=0;
                 exit(0);
             }else if(strcmp(args[0],"cd")==0){
-                cambiarDirectorio();
+                ejecutarCD();
+		printf("Estoy en %s \n",sd_actual.name);
+		printf("Length actual %d \n",sd_actual.size);
             }else if(strcmp(args[0],"editor")==0){
                 editor();
             }else{
@@ -142,18 +146,72 @@ void separarArgumentos(){
     }
 }
 
-void cambiarDirectorio(){
+/*1 el cliente solicita a la shell ejecutar cd con el nombre de la carpeta N que desea acceder [COM5]
+2 el nodo le pregunta al coordinador si la direccion N es valido [COM3Y6]
+3 el coordinador utiliza su tabla para saber si N es una direccion valida para ser accedida [COM8][COM4]
+4 si la ruta es valida el coordinador retorna true, false en caso contrario [COM8][COM4]
+5 en caso de recibir false muestra por pantalla mensaje de error [COM5]
+6 en caso de recibir true, el nodo accede al directorio NR y actualiza su dirActual[COM5]
+*/
+
+
+void ejecutarCD(){
+    if(args[1]==NULL){
+	printf("uso: cd directorio \n");
+    }else //chequear ..
+    {
+	if(strcmp(args[1], "..")==0){
+	    if(strcmp(sd_actual.name,"raiz")){
+		
+		strcpy(sd_actual.name,sd_padre.name);
+		sd_actual.size = sd_padre.size;
+		printf("sd_actual en el .. es %s \n",sd_actual.name);
+
+		//ruta.sub_directorios[1] = NULL;
+	    }else{
+		printf("No se pueda ir al padre de la raiz \n");
+	    }
+	}else{
+	    //Pregunta al coordinador si es valido un directorio con 0.
+	    
+	    char contenido_mensaje[1+sd_actual.size];
+	    strcpy(contenido_mensaje,"0");
+	    strcat(contenido_mensaje,sd_actual.name);
+
+
+	     Mensaje cd =
+	    {
+		    sd_actual.size,
+		    contenido_mensaje,
+	    };
+	    //int valid = is_valid_1(&cd,clnt);
+	    int valid = 1;
+	    if(valid){
+		sd_actual.size = strlen(args[1]);
+		strcpy(sd_actual.name,args[1]);
+		printf("sd_actual %s \n",sd_actual.name);
+		//manejar arreglo de sub_directorios
+		ruta.cont = ruta.cont ++;
+		ruta.sub_directorios[1] = sd_actual;
+		
+	    }else{
+		printf("El directorio ingresado no existe \n");
+	    }
+	}
+    }
 }
+
 
 void editor(){
 }
 
 void listarDirectorio(){
+    printf("Estoy preguntando por esta carpeta %s\n",sd_actual.name);
 
     Mensaje msg_test =
     {
-	    sd_actual.size,
-	    sd_actual.name,
+	   sd_actual.size,
+	   sd_actual.name,
     };
     
     
