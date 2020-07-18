b8db9df5537bf1e0afe47919578edfcf9f5a8eb5
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2020-07-18 20:39:01 -0300

    remove

---
+++ protocolo.x
@@ -0,0 +1,17 @@
+typedef char Mensaje<>;
+
+program PROY2DFS
+{
+	version PROY2DFSVERS
+	{
+		Mensaje LS(Mensaje)=1;
+		Mensaje GETADDRESS(Mensaje)=2;
+		int IS_VALID(Mensaje)=3;
+		Mensaje UPDATE_ADDRESS(Mensaje)=4;
+		Mensaje REPORT_CREATE(Mensaje)=5;
+		Mensaje REPORT_DELETE(Mensaje)=6;
+		int IS_EMPTY(Mensaje)=7;
+	}=1;
+	
+}=0x20001003;
+
---
+++ servidor.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "protocolo.h"
+#include "coordinador.h"
+
+Mensaje *ls_1_svc(Mensaje *msg, struct svc_req *req)
+{	
+	static Mensaje to_return;
+	to_return.Mensaje_val = funcionListar(msg->Mensaje_val);
+	to_return.Mensaje_len = 1 + strlen(to_return.Mensaje_val);
+	return (&to_return);	
+}
+
+Mensaje *getaddress_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	static Mensaje to_return;
+	to_return.Mensaje_val = obtenerIP(msg->Mensaje_val);
+	to_return.Mensaje_len = 1 + strlen(to_return.Mensaje_val);
+	return (&to_return);
+}
+
+int *is_valid_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	static int to_return;
+	to_return=1;//esValido(msg->Mensaje_val);
+	return &to_return;
+}
+
+Mensaje *update_address_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	static Mensaje to_return;
+	return(&to_return);
+}
+
+Mensaje *report_create_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	static Mensaje to_return;
+	return(&to_return);
+}
+
+Mensaje *report_delete_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	printf("Reporto el delete de %s.\n",msg->Mensaje_val);
+	char str[64];
+	strcpy(str,"");
+	char* aux = "Terminado prro";
+	strcat(str, aux);
+	static Mensaje to_return;
+	
+	to_return.Mensaje_len =	64;
+	to_return.Mensaje_val =	str;
+	
+	return (&to_return);
+}
+
+int *is_empty_1_svc(Mensaje *msg, struct svc_req *req)
+{
+	static int to_return;
+	to_return=1;
+	return &to_return;
+}
+
