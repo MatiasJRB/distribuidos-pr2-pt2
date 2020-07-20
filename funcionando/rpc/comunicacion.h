8f54ea5ba39420f5750db5e3ec9194ae30e2404d
nlandau91 <nlandau91@gmail.com>
2020-07-19 21:46:09 -0300

    smg

---
+++ funcionando/rpc/comunicacion.h
@@ -0,0 +1,12 @@
+#ifndef COMUNICACION_H_
+#define COMUNICACION_H_
+
+	char* ls(char* dir);
+	char* getaddress(char* archivo, char* ubicacion);
+	int exists(char tipo, char* nombre; char* ubicacion);
+	int report_create(char tipo, char* nombre, char* ip, char* ubicacion);
+	int report_delete(char tipo, char* nombre, char* ip, char* ubicacion);
+	int is_empty(char* nombre);
+
+
+#endif
