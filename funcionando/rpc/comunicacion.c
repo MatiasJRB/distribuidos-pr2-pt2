88006b67b37e25f0cfe1d588852a0e534c339299
nlandau91 <nlandau91@gmail.com>
2020-07-20 00:34:36 -0300

    quiero dormir

--- funcionando/rpc/comunicacion.c
+++ funcionando/rpc/comunicacion.c
@@ -82,13 +82,47 @@ int report_create(CLIENT* clnt, char tipo, char* nombre, char* ip, char* ubicaci
         1 + strlen(buf),
         buf,
     };
-    int to_return = *exists_1(&to_send,clnt);
+    int to_return = *report_create_1(&to_send,clnt);
     return to_return;
 }
 
 int report_delete(CLIENT* clnt, char tipo, char* nombre, char* ip, char* ubicacion)
 {
-    return 0;
+    int size = 4 + strlen(nombre) + strlen(ip);
+    if(ubicacion != NULL)
+    {
+        size = size + 1 + strlen(ubicacion);
+    }
+    char buf[size];
+    buf[0] = tipo;
+    buf[1] = '\0';
+    strcat(buf,",");
+    strcat(buf,nombre);
+    strcat(buf,",");
+    strcat(buf,ip);
+    if(ubicacion != NULL)
+    {
+        strcat(buf,",");
+        strcat(buf,ubicacion);   
+    }
+    Mensaje to_send = 
+    {
+        1 + strlen(buf),
+        buf,
+    };
+    int to_return = *report_delete_1(&to_send,clnt);
+    return to_return;
+}
+
+int is_empty(CLIENT* clnt, char* nombre)
+{
+	Mensaje to_send =
+    {
+        1 + strlen(nombre),
+        nombre,
+    };
+    int rcv = *is_empty_1(&to_send, clnt);
+    return rcv;
 }
 
 
